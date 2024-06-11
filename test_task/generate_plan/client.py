333b244921caab11f11b60e9f72b9fc9a3973f0a
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-06-11 17:51:13 +0600

    f

--- test_task/generate_plan/client.py
+++ test_task/generate_plan/client.py
@@ -1,7 +1,11 @@
+import httpx
 from django.conf import settings
+from django.contrib.sites import requests
 from openai import OpenAI
 
+proxy = '152.26.229.86:9443'
 
 OPENAI_API_KEY = settings.OPENAI_API_KEY
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY,
+                http_client=httpx.Client(proxy=proxy))
