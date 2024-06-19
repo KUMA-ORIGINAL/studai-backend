01d4e05bc54ee99ec08a8d591716430948ace7ae
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-06-19 19:20:00 +0600

    big commit

---
+++ app/account/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+# from account import views
+
+router = DefaultRouter()
+#
+# router.register(r'users/favorite',
+#                 views.FavoriteArticlesViewSet,
+#                 basename='favorite-articles')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
+]
